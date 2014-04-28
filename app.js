1c621065585e828bb4235f49edcd4e48c5b1ee68
Jeffry Ghazally <jghazally@screamingcodemonkey.com>
2014-04-28 20:18:54 +1000

    Merge var calls into one

--- app.js
+++ app.js
@@ -1,13 +1,12 @@
-var secrets = require('./secrets.js');
-var _ = require('underscore');
-var request = require('request');
-var ninjaBlocks = require('ninja-blocks');
+var secrets = require('./secrets.js'),
+	_ = require('underscore'),
+	request = require('request'),
+	ninjaBlocks = require('ninja-blocks');
 
 var ninja = ninjaBlocks.app({user_access_token:secrets.USER_ACCESS_TOKEN});
-var recentQuake = 0;
-var magnitude = 0;
-
-var colors = {
+	recentQuake = 0,
+	magnitude = 0,
+	colors = {
 	0: '8f00ff', // Violet
 	1: '4b0082', // Indigo
 	2: '0000ff', // Blue
@@ -35,9 +34,10 @@ function checkGeonet() {
 function changeColor() {
 	ninja.devices({ device_type: 'rgbled' }, function(err, devices) {
 		_.each(devices, function(device, guid) {
+			console.log('NZ Hit by a ' + magnitude + 'M quake');
 			ninja.device(guid).actuate(colors[magnitude]);
 		});
 	});
 }
 
-setInterval(checkGeonet, 3000);
+setInterval(checkGeonet, 5000);
